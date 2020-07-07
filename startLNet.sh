4850a48057be3a2af23cc7ae392681b36ca2491e
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-07-07 19:01:06 +0530

    Added set flags to start and stop LNet

--- startLNet.sh
+++ startLNet.sh
@@ -1,3 +1,4 @@
+set -e # Stop the script if any error occurs
 ./myscripts/create-crypto.sh
 ./myscripts/create-genesis.sh
 docker-compose down; docker volume prune -f; docker-compose up
--- stopLNet.sh
+++ stopLNet.sh
@@ -1,3 +1,4 @@
+set -e # Stop the script if any error occurs
 sudo rm -r ../{crypto,fabric,crypto-config,channel-artifacts/*};
 docker-compose down; 
 docker volume prune -f;
